70428cd3de72249b121142e0f18a9446b6c71c41
tnp2004 <iceandiceisice2004@gmail.com>
2024-06-09 16:04:42 +0700

    :zap: Check win condition

--- game.rb
+++ game.rb
@@ -49,3 +49,34 @@ def GetPlayerSymbol(playerNumber)
         puts "Invalid player number"
     end
 end
+
+$winCombinations = [ 
+[0,1,2], # top row 
+[3,4,5], # middle row 
+[6,7,8], # bottom row 
+[0,3,6], # left column 
+[1,4,7], # center column 
+[2,5,8], # right column 
+[0,4,8], # left diagonal 
+[6,4,2] # right diagonal 
+]
+
+def IsWin(table)
+    table.each do |row|
+        return row[0] if row.uniq.length == 1 && row[0] != nil
+      end
+    
+      # Check columns
+      (0..2).each do |col|
+        column = [table[0][col], table[1][col], table[2][col]]
+        return column[0] if column.uniq.length == 1 && column[0] != nil
+      end
+    
+      # Check diagonals
+      diagonal1 = [table[0][0], table[1][1], table[2][2]]
+      diagonal2 = [table[0][2], table[1][1], table[2][0]]
+      return diagonal1[0] if diagonal1.uniq.length == 1 && diagonal1[0] != nil
+      return diagonal2[0] if diagonal2.uniq.length == 1 && diagonal2[0] != nil
+    
+      return nil
+end
--- main.rb
+++ main.rb
@@ -4,20 +4,28 @@ def StartGame
     puts "Welcome to Tic-Tac-Toe Command Line"
     # Create table
     maxTableRows = 3
+    maxTableColumns = 3
     playerX = 1
     playerO = 2
-    tableArr = Array.new(maxTableRows) { Array.new(maxTableRows) }
-
+    tableArr = Array.new(maxTableColumns) { Array.new(maxTableRows) }
+    rounds = maxTableColumns * maxTableRows
     # 7 8 9
     # 4 5 6
     # 1 2 3
-    9.times {|i|
+    rounds.times {|i|
         playerNumber = i%2===0 ? playerX : playerO
         playerSymbol = playerNumber%2 != 0 ? "X" : "O"
         print "Player #{playerSymbol} Select cell: "
         selectedCell = gets.to_i
         AssignCell(tableArr, playerNumber, selectedCell)
+        winner = IsWin(tableArr)
+        if winner != nil
+            puts "Winner: #{winner}"
+            exit
+        end
     }
+
+    puts "Draw"
 end
 
 StartGame()
